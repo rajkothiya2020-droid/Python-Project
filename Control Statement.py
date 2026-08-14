58fab955dcae8060dc79456b76b7e7b1612980f0
rajkothiya <rajkothiya2020@gmail.com>
2026-08-14 12:04:09 +0530

    Control Statement

---
+++ Control Statement.py
@@ -0,0 +1,51 @@
+data = input("Enter data: ")
+
+if (data == "Admin"):
+    print("Data matches Admin")
+else:
+    print("Data does not match Admin")
+
+
+
+
+
+
+
+
+
+
+num = int(input("Enter a number: "))
+
+if num == 0:
+    print("The given number is Zero")
+else:
+    print("The given number is Not Zero")
+
+
+
+
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+
+
+
+
+
+
+
+
+
+num = int(input("Enter a number: "))
+
+if num >= 1:
+    print("The given number is Natural")
+else:
+    print("The given number is Not Natural")
+    
